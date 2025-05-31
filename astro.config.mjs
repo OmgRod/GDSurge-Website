52e635a2b2f2c914eb12cfca2142d98d1608565b
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-05-31 14:19:40 +0100

    added install page (to be written), and removed bloat

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,26 +3,17 @@ import starlight from '@astrojs/starlight';
 import catppuccin from "@catppuccin/starlight";
 
 export default defineConfig({
+	site: 'https://gdsurge.omgrod.me',
+	base: '/',
 	integrations: [
 		starlight({
 			title: 'GD Surge',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/OmgRod/GD-Surge' }],
-			// sidebar: [
-			// 	{
-			// 		label: 'Guides',
-			// 		items: [
-			// 			{ label: 'Example Guide', slug: 'guides/example' },
-			// 		],
-			// 	},
-			// 	{
-			// 		label: 'Reference',
-			// 		autogenerate: { directory: 'reference' },
-			// 	},
-			// ],
 			plugins: [catppuccin({
 				dark: 'mocha',
 				light: 'latte',
 			})],
+			pagefind: false,
 		}),
 	],
 });
